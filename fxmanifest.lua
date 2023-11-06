99ed6a62e88fb12fdfe2290ad7c4b55e41a1c348
frackz <65610875+frackz@users.noreply.github.com>
2023-11-06 16:26:04 +0100

    Resource

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,18 @@
-fx_version 'cerulean'
-game 'gta5'
+-- ############### --
+--     FX INFO     --
+-- ############### --
+fx_version  'cerulean'
+game        'gta5'
+lua54       'yes'
 
+name        'Development'
+author      'frackz'
+version     '1.0'
+license     'GPL v3.0'
+repository  'https://github.com/spark-framework/development'
+description 'Resource utilized for developing on Spark'
+
+-- ############### --
+--     SCRIPTS     --
+-- ############### --
 server_script 'server/main.lua'
